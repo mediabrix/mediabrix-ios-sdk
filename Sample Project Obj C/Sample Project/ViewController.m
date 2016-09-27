711b7920dd2809c8e680a610daa1ebec9ab2975f
koraybrix <koray@mediabrix.com>
2016-09-27 11:59:27 -0400

    9 26 release (#4)
    * Fixing test app, and adding new mediabrix proj
    
    * updating swift project as well
    
    * removing dispatch async
    
    * updating swift project
    
    * updating wiki
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md

--- Sample Project Obj C/Sample Project/ViewController.m
+++ Sample Project Obj C/Sample Project/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <MediaBrixDelegate>
 
 @end
 
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [MediaBrix initMediaBrixAdHandler:self withBaseURL:@"http://mobile.mediabrix.com/v2/manifest" withAppID:@"TwwvxoFnJn"];
+    [MediaBrix initMediaBrixDelegate:self withBaseURL:@"http://mobile.mediabrix.com/v2/manifest" withAppID:@"TwwvxoFnJn"];
     [self loadAd];
 
 }
@@ -26,56 +26,23 @@
     [[MediaBrix sharedInstance]loadAdWithIdentifier:@"Babel_Rally" adData:self.publisherVars withViewController:self];
 }
 
+-(void)showAd {
+    [[MediaBrix sharedInstance]showAdWithIdentifier:@"Babel_Rally" fromViewController:self reloadWhenFinish:NO];
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-- (void)mediaBrixAdHandler:(NSNotification *)notification {
-    
-    /* implement the "mediaBrixAdHanlder" function for notifications (Required) */
-    if ([kMediaBrixStarted isEqualToString:notification.name]) {
-        [self loadAd];
-    }
-    else if([kMediaBrixAdWillLoadNotification isEqualToString:notification.name]){
-        
-        /* invoked when the ad has been requested */
-        
-    }
-    else if([kMediaBrixAdFailedNotification isEqualToString:notification.name]){
-        
-        
-        /* invoked whne ad failure occurs */
-    }
-    else if([kMediaBrixAdReadyNotification isEqualToString:notification.name]){
-        
-        //[[MediaBrix sharedInstance]showAdWithIdentifier:@"IOS_Rescue" fromViewController:self reloadWhenFinish:NO];
-        dispatch_async(dispatch_get_main_queue(), ^{
-            // do work here
-            
-            [[MediaBrix sharedInstance]showAdWithIdentifier:@"Babel_Rally" fromViewController:self reloadWhenFinish:NO];
-        });
-        //
-        /* invoked when ad has succesfully downloaded and ready for showing */
-    }
-    else if([kMediaBrixAdShowNotification isEqualToString:notification.name]){
-        
-        /* invoked when ad is presented */
-        
-    }
-    else if([kMediaBrixAdDidCloseNotification isEqualToString:notification.name]){
-        
-        /* invoked when ad is closed */
-    }
-    else if([ kMediaBrixAdRewardNotification isEqualToString:notification.name]){
-        
-        /* invoked when ad view is completed and reward can be given */
-    }else if([ kMediaBrixAdClickedNotification isEqualToString:notification.name]){
-        NSLog(@"ADCLICKED");
-        /* invoked when ad view is completed and reward can be given */
-    }
-    
-}
 
+#pragma mark - MediaBrixDelegate
+
+-(void)mediaBrixStarted {
+    [self loadAd];
+}
+-(void)mediaBrixAdReady:(NSString *)identifier {
+    [self showAd];
+}
 
 @end
